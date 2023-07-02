b5e5e6c28d2c4fb7a75201f56c0f465c885a093d
Irina_Leskova <krip@mail.ru>
2023-07-02 19:55:09 +0500

    Сделала 3 задание!

---
+++ z3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("введите номер числа недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a ==1){Console.WriteLine("Это понедельник");}
+if (a ==2){Console.WriteLine("Это вторник");}
+if (a ==3){Console.WriteLine("Это среда");}
+if (a ==4){Console.WriteLine("Это четверг");}
+if (a ==5){Console.WriteLine("Это пятница");}
+if (a ==6){Console.WriteLine("Это суббота");}
+if (a ==7){Console.WriteLine("Это воскресенье");}
+else if (a<1||a>7){Console.WriteLine("Это не день недели");}
