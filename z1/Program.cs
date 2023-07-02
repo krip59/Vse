9314295fcb6fdb546d00c1c89c2ba1a0f20c1e0d
Irina_Leskova <krip@mail.ru>
2023-07-02 19:40:44 +0500

    Сделала 1 задание!

---
+++ z1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if ((numbera == numberb*numberb)|(numberb == numbera*numbera))
+{
+Console.Write("Да");
+}
+else {Console.Write("Нет");
+}
